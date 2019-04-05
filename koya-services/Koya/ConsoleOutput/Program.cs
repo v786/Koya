3aa4d59c3fbd73663ff1d3b175565745a188cff6
munish7771 <munish7771@gmail.com>
2019-04-05 19:20:47 +0530

    Initial set-up
    no such

---
+++ koya-services/Koya/ConsoleOutput/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
